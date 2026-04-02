af71985c0dfa3389a25eafcdfa8f3adbaa233592
rajesh <rajesh.mandal123@gmail.com>
2026-04-02 18:29:28 +0530

    changes on about us section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,41 +1,96 @@
 import "./Services.css";
+import cloud from "../../assets/cloud.png"
+import infra from "../../assets/infra.png"
+import software from "../../assets/software.jpg"
+import datacenter from '../../assets/datacenter.png'
+function Services() {
+  return (
+    <section className="services">
+      <h2>What We Do</h2>
 
-function Services(){
+      <div className="cards">
+        <div className="card">
+          <div className="upper_section">
+           <img src={cloud} alt="Cloud computing" className="image"  />
+            <div className="icon">
+              <i className="fa-solid fa-cloud"></i>
+            </div>
+          </div>
 
-return(
+          <div className="lower_section">
+            <h3>Cloud Solutions</h3>
+            <p>
+              Powerful cloud infrastructure services designed to scale your
+              business and accelerate digital transformation.
+            </p>
 
-<section className="services">
+            <a href="#" className="btn">
+              Learn More <i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="upper_section">
+             <img src={infra} alt="infra" className="image"  />
+            <div className="icon">
+              <i class="fa-solid fa-building"></i>
+            </div>
+          </div>
+          <div className="lower_section">
+            <h3>Infrastructure Services</h3>
+            <p>
+              Powerful cloud infrastructure services designed to scale your
+              business and accelerate digital transformation.
+            </p>
 
-<h2>What We Do</h2>
+            <a href="#" className="btn">
+              Learn More <i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="upper_section">
+            <img src={software} alt="infra" className="image"  />
+            <div className="icon">
+             <i class="fa-solid fa-code"></i>
+            </div>
+          </div>
 
-<div className="cards">
+          <div className="lower_section">
+            <h3>Software Services</h3>
+            <p>
+              Powerful cloud infrastructure services designed to scale your
+              business and accelerate digital transformation.
+            </p>
 
-<div className="card">
-<h3>AI Solutions</h3>
-<p>AI systems for business automation.</p>
-</div>
+            <a href="#" className="btn">
+              Learn More <i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="upper_section">
+            <img src={datacenter} alt="data-center" className="image"  />
+            <div className="icon">
+            <i class="fa-solid fa-server"></i>
+            </div>
+          </div>
 
-<div className="card">
-<h3>Software Development</h3>
-<p>Custom web and mobile apps.</p>
-</div>
+          <div className="lower_section">
+            <h3>Data Center Solution</h3>
+            <p>
+              Powerful cloud infrastructure services designed to scale your
+              business and accelerate digital transformation.
+            </p>
 
-<div className="card">
-<h3>Automation</h3>
-<p>Improve productivity with automation.</p>
-</div>
-
-<div className="card">
-<h3>Data Analytics</h3>
-<p>Convert data into insights.</p>
-</div>
-
-</div>
-
-</section>
-
-)
-
+            <a href="#" className="btn">
+              Learn More <i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
